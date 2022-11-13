b730485b926aa73270d9d4f7857e62462ec47a5f
Shirendev <shirendev.07@gmail.com>
2022-11-13 12:39:38 +0800

    he

---
+++ eng/src/App.js
@@ -0,0 +1,152 @@
+import { Container, Switch, withStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
+import { createMuiTheme,TextField, ThemeProvider } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { debounce } from "lodash";
+import axios from "axios";
+
+
+function App() {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState("en");
+  const [LightTheme, setLightTheme] = useState(false);
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`
+      );
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+    // eslint-disable-next-line
+  }, [word, category]);
+ 
+  const darkTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: LightTheme ? "#000" : "#fff",
+      },
+      type: LightTheme ? "light" : "dark",
+    },
+  });
+
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+    setWord("");
+    setMeanings([]);
+  };
+
+    const handleText = debounce((text) => {
+    setWord(text);
+  }, 500);
+  
+
+
+
+
+  const PurpleSwitch = withStyles({
+    switchBase: {
+      color: grey[50],
+      "&$checked": {
+        color: grey[900],
+      },
+      "&$checked + $track": {
+        backgroundColor: grey[500],
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
+  return (
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        backgroundColor: LightTheme ? "#fff" : "#282c34",
+        color: LightTheme ? "black" : "white",
+        transition: "all 0.5s linear",
+      }}
+    >
+     
+        <div
+          style={{ position: "absolute", top: 0, right: 15, paddingTop: 10 }}
+        >
+          <span>{LightTheme ? "Dark" : "Light"} Mode</span>
+          <PurpleSwitch
+            checked={LightTheme}
+            onChange={() => setLightTheme(!LightTheme)}
+          />
+        </div>
+        <div className="header" setWord={setWord}
+          category={category}
+          setCategory={setCategory}
+          word={word}
+          setMeanings={setMeanings}
+          LightTheme={LightTheme}>
+      <span className="title">{word ? word : "Word Hunt"}</span>
+      <div className="inputs">
+      <ThemeProvider>
+          <TextField
+            className="search"
+            id="filled-basic"
+            // value={word}
+            label="Search a Word"
+            onChange={(e) => handleText(e.target.value)}
+          />
+          <TextField>
+          
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+    <div className="meanings" meanings={meanings}
+            word={word}
+            LightTheme={LightTheme}
+            category={category}>
+    
+
+    {word === "" ? (
+        <span className="subTitle">Start by typing a word in search</span>
+      ) : (
+        meanings.map((mean) =>
+          mean.meanings.map((item) =>
+            item.definitions.map((def) => (
+              <div
+                className="singleMean"
+                style={{
+                  backgroundColor: LightTheme ? "#3b5360" : "white",
+                  color: LightTheme ? "white" : "black",
+                }}
+              >
+                <b>  <ul>{def.definition}</ul></b>
+                <hr style={{ backgroundColor: "black", width: "100%" }} />
+                {def.example && (
+                  <span>
+                    <ul> <b>Example :</b> {def.example}</ul>
+                  </span>
+                )}
+               
+              </div>
+            ))
+          )
+        )
+      )}
+               
+              
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
--- gms/src/App.js
+++ gms/src/App.js
@@ -1,127 +1,53 @@
-import { useEffect, useState } from 'react'
-import TextBox from './components/TextBox'
-import Arrows from './components/Arrows'
-import Button from './components/Button'
-import Modal from './components/Modal'
-import axios from 'axios'
+import {useState} from 'react';
 
 const App = () => {
-  const [showModal, setShowModal] = useState(false)
-  const [languages, setLanguages] = useState(null)
-  const [inputLanguage, setInputLanguage] = useState('English')
-  const [outputLanguage, setOutputLanguage] = useState('Polish')
-  const [textToTranslate, setTextToTranslate] = useState('')
-  const [translatedText, setTranslatedText] = useState('')
+  const [message, setMessage] = useState('');
+  const [meaning, setMeaning] = useState('');
 
+  const handleChange = event => {
+    setMessage(event.target.value);
 
-  const getLanguages = async () => {
-
-  const options = {
-    method: 'GET',
-    url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages',
-    headers: {
-      'Accept-Encoding': 'application/gzip',
-      'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
-      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    }
+    console.log('value is:', event.target.value);
   };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-    const arrayOfData = Object.keys(response.data.data).map(key => response.data.data[key])
-      setLanguages(arrayOfData)
-  }).catch(function (error) {
-    console.error(error);
-  });}
 
+  const translate = () => {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("q", message);
+    encodedParams.append("target", "mn");
+    encodedParams.append("source", "en");
+    
+    const translateApiOptions = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': 'e1b75410aemsh25be6f5e579bb8ap15bdacjsn098f8f344007',
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+      },
+      body: encodedParams
+    };
+    
+    return fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', translateApiOptions)
+      .then(response => response.json())
+      .then(response => setMeaning(response.data.translations[0].translatedText))
+      .catch(err => console.error(err));
 
-
-
-
-
-
-
-const translate = async () => {
-  
-
-  const encodedParams = new URLSearchParams();
-  encodedParams.append("q", "Hello, world!");
-  encodedParams.append("target", "es");
-  encodedParams.append("source", "en");
-  
-  const options = {
-    method: 'POST',
-    url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Accept-Encoding': 'application/gzip',
-      'X-RapidAPI-Key': 'c0a2d3b7a8mshe348abd873a4e4ep17385djsn44c42053c9a5',
-      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
-    },
-    data: encodedParams
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-    setTranslatedText(response.data)
-  }).catch(function (error) {
-    console.error(error);
-  });
-}
-
-  
-
-useEffect(()=>{
-  getLanguages()
-},[])
-  const handleClick = () => {
-    setInputLanguage(outputLanguage)
-    setOutputLanguage(inputLanguage)
   }
-
+  console.log()
 
   return (
-    <div className="app">
-      {!showModal && (
-        <>
-          <TextBox
-            style="input"
-            setShowModal={setShowModal}
-            selectedLanguage={inputLanguage}
-            setTextToTranslate={setTextToTranslate}
-            textToTranslate={textToTranslate}
-            setTranslatedText={setTranslatedText}
-          />
-          <div className="arrow-container" onClick={handleClick}>
-            <Arrows />
-          </div>
-          <TextBox
-            style="output"
-            setShowModal={setShowModal}
-            selectedLanguage={outputLanguage}
-            translatedText={translatedText}
-          />
-          <div className="button-container" onClick={translate}>
-            <Button />
-          </div>
-        </>
-      )}
-      {showModal && (
-        <Modal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          languages={languages}
-          chosenLanguage={
-            showModal === 'input' ? inputLanguage : outputLanguage
-          }
-          setChosenLanguage={
-            showModal === 'input' ? setInputLanguage : setOutputLanguage
-          }
-        />
-      )}
+    <div>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        onChange={handleChange}
+        value={message}
+      />
+
+      <h2 onClick={translate}>Message:{meaning} </h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
